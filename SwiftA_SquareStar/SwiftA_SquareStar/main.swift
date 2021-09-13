8cb536a124ba77d99f713845ff3089f76524bce8
delight010 <81559026+delight010@users.noreply.github.com>
2021-09-13 17:24:13 +0900

    0913 직사각형 별찍기
    0913 직사각형 별찍기

---
+++ SwiftA_SquareStar/SwiftA_SquareStar/main.swift
@@ -0,0 +1,34 @@
+//
+//  main.swift
+//  SwiftA_SquareStar
+//
+//  Created by Seong A Oh on 2021/09/13.
+//
+// 이 문제에는 표준 입력으로 두 개의 정수 n과 m이 주어집니다.
+// 별(*) 문자를 이용해 가로의 길이가 n, 세로의 길이가 m인 직사각형 형태를 출력해보세요.
+//
+// < 제한 조건 >
+// - n과 m은 각각 1000 이하인 자연수입니다.
+//
+// < 예시 >
+// 입력 5 3
+// 출력
+// *****
+// *****
+// *****
+
+import Foundation
+
+let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
+let (a, b) = (n[0], n[1])
+
+
+for _ in 0..<b{
+    let star = String(repeating: "*", count: a)
+    print(star)
+}
+
+// String(repeating: String, count: Int)
+
+
+// 소요시간 11분
