58ff388375ec17fbc8de0b0b6b50d5b4b2885a51
delight010 <81559026+delight010@users.noreply.github.com>
2021-10-21 20:02:26 +0900

    1021_나머지가 1이 되는 수 찾기
    소요시간 9분

---
+++ SwiftA_Remain1Number/SwiftA_Remain1Number/main.swift
@@ -0,0 +1,38 @@
+//
+//  main.swift
+//  SwiftA_Remain1Number
+//
+//  Created by Seong A Oh on 2021/10/21.
+//
+// 자연수 n이 매개변수로 주어집니다.
+// n을 x로 나눈 나머지가 1이 되도록 하는 가장 작은 자연수 x를 return 하도록 solution 함수를 완성해주세요.
+// 답이 항상 존재함은 증명될 수 있습니다.
+//
+// < 제한 사항 >
+// 3 <= n <= 1,000,000
+//
+// < 입출력 예 >
+// n = 10
+// result = 3
+// 10을 3으로 나눈 나머지가 1이고, 3보다 작은 자연수 중에서 문제의 조건을 만족하는 수가 없으므로, 3을 return 해야 합니다.
+//
+// n = 12
+// result = 11
+// 12를 11로 나눈 나머지가 1이고, 11보다 작은 자연수 중에서 문제의 조건을 만족하는 수가 없으므로, 11을 return 해야 합니다
+
+import Foundation
+
+func solution(_ n:Int) -> Int {
+    for i in 2..<n {
+        if n % i == 1 {
+            return i
+        }
+    }
+    return n-1
+}
+
+// 소요시간 9분
+
+print(solution(10))
+print(solution(12))
+
