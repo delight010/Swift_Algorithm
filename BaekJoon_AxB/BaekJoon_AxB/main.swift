430d96a89d56cd20767c0bca900cb33629f921a5
delight010 <81559026+delight010@users.noreply.github.com>
2023-09-16 21:46:53 +0900

    백준 알고리즘 A*B, A/B

---
+++ BaekJoon_AxB/BaekJoon_AxB/main.swift
@@ -0,0 +1,24 @@
+//
+//  main.swift
+//  BaekJoon_AxB
+//
+//  Created by Seong A Oh on 2023/09/16.
+//
+
+import Foundation
+// [입출력과 사칙연산] A×B
+// 두 정수 A와 B를 입력받은 다음, A×B를 출력하는 프로그램을 작성하시오.
+
+let inputInteger = readLine()
+if let integers = inputInteger {
+    let solve = integers.components(separatedBy: " ").compactMap { char -> Int in
+        guard let integer = Int(char) else { return 0 }
+        return integer
+    }
+    
+    let result = solve.reduce(1) { partialResult, nextInteger in
+        partialResult * nextInteger
+    }
+    
+    print(result)
+}
